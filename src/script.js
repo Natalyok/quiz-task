f8d341134fcab593d531f51cb3dd29fa970641c8
i_babybone <nshalimo6@gmail.com>
2024-09-28 16:23:53 +0300

    feat: button activity

--- src/script.js
+++ src/script.js
@@ -2,18 +2,62 @@ const start = document.querySelector('.start-button')
 const wrapper = document.querySelector('.start-wrapper')
 const quiz = document.querySelector('.quiz-wrapper')
 const questionHeading = document.querySelector('.question-heading')
+const resultHeading = document.querySelector('.result-heading')
+
+let overallCounter = 0
+let correctCounter = 0
+let wrongCounter = 0
+
+function countAnswers(answer, button) {
+    return function () {
+        overallCounter += 1
+
+        if (String(answer) === button.innerText) {
+            correctCounter += 1
+        } else {
+            wrongCounter += 1
+        }
+    }
+}
 
 
-function generateCorrectButton() {
+function showResult() {
+    wrapper.classList.remove('hide')
+    quiz.classList.add('hide')
+    resultHeading.classList.remove('hide')
+    resultHeading.innerText = 'yo'
+    resultHeading.innerText = `overall answers: ${overallCounter}, correct: ${correctCounter}, wrong ${wrongCounter} `
+}
+
+function generateCorrectButton(answer) {
     const buttons = document.querySelectorAll(".answer-row button")
-    const index = getRandomInt(buttons.length)
-    const correctButton = buttons[index]
-    correctButton.innerText = 'correct'
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', countAnswers(answer, button))
+    })
 
 
     buttons.forEach((button) => {
-        button.addEventLister('click', generateQuestion)
+        button.addEventListener('click', generatOuastion)
+
+        let value = getRandomInt(100)
+        while (value === answer) {
+            value = getRandomInt(100)
+        }
+        button.innerText = value
+        button.classList.remove('correct-button')
+        button.classList.add('wrong-button')
     })
+
+
+
+    const index = getRandomInt(buttons.length)
+    const correctButton = buttons[index]
+    correctButton.innerText = answer
+    correctButton.classList.remove('wrong-button')
+    correctButton.classList.add('correct-button')
+
+
 }
 
 
@@ -21,6 +65,7 @@ function startQuiz() {
     wrapper.classList.add('hide')
     quiz.classList.remove('hide')
     generatOuastion()
+    setTimeout(showResult, 10000)
 }
 
 function getRandomInt(max) {
@@ -30,9 +75,16 @@ function generatOuastion() {
     const number1 = getRandomInt(50)
     const number2 = getRandomInt(50)
     const sign = lol()
+    let answer = ''
+
+    if (sign === '+') {
+        answer = number1 + number2
+    } else {
+        answer = number1 - number2
+    }
 
     questionHeading.innerText = `${number1} ${sign} ${number2}`
-    generateCorrectButton()
+    generateCorrectButton(answer)
 }
 function lol() {
     if (getRandomInt(2) === 0) {
